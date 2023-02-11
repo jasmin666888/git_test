40ee8613364a5af20ef8c7d4a44b58b4201fe6f8
jasmin <850489301@qq.com>
2023-02-11 22:10:06 +0800

    hot-fix commit2

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -3,5 +3,6 @@ public class Test1 {
         System.out.println("git2");
         System.out.println("git3");
         System.out.println("git4");
+        System.out.println("hot-fix test2");
     }
 }
